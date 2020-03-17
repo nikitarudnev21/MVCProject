808851b2f74917e6aebf2907a52c0f52f3f9b765
Nikita <styx1338@gmail.com>
2020-03-17 12:26:08 +0200

    first version

---
+++ MVCRudnev/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCRudnev.Models;
+
+namespace MVCRudnev.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // возвращаем представление
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            var list = books.ToList();
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return ViewBag.BookName =  "<h2>" + "Спасибо, " + purchase.Person + ", за покупку! "+ "</h2>"  +"ты купил книгу: " + 
+                "<br>" + " Автор: " + list[purchase.BookId - 1].Author
+                + "<br>" + " Название книги:"+ list[purchase.BookId-1].Name + 
+                "<br>"  + "Она стоила: " + list[purchase.BookId - 1].Price +  "евро"
+                +"<br>" + " Время и дата покупки: " + purchase.Date
+                + "<br>" + " Книга будет доставлена на адресс: " + purchase.Address;
+        }
+    }
+}
---
+++ MVCRudnev/Models/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRudnev.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+        // имя и фамилия покупателя
+        public string Person { get; set; }
+        // адрес покупателя
+        public string Address { get; set; }
+        // ID книги
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+
+        /*Book book = new Book();
+        public string BookName
+        {
+            get { return  book.Name; }   
+            set { book.Name = book.Name; }  
+        }*/
+        // дата покупки
+        public DateTime Date { get; set; }
+    }
+}
