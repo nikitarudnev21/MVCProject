12dda7728114ad0f3735e283abf1f3969888ee5d
Nikita <styx1338@gmail.com>
2020-03-17 15:07:26 +0200

    second version

--- MVCRudnev/Controllers/HomeController.cs
+++ MVCRudnev/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -35,12 +36,64 @@ namespace MVCRudnev.Controllers
             purchase.Date = DateTime.Now;
             db.Purchases.Add(purchase);
             db.SaveChanges();
-            return ViewBag.BookName =  "<h2>" + "Спасибо, " + purchase.Person + ", за покупку! "+ "</h2>"  +"ты купил книгу: " + 
-                "<br>" + " Автор: " + list[purchase.BookId - 1].Author
-                + "<br>" + " Название книги:"+ list[purchase.BookId-1].Name + 
-                "<br>"  + "Она стоила: " + list[purchase.BookId - 1].Price +  "евро"
-                +"<br>" + " Время и дата покупки: " + purchase.Date
-                + "<br>" + " Книга будет доставлена на адресс: " + purchase.Address;
+            return ViewBag.BookName = 
+                "<style> *{background-color: lightgray}</style>"
+                + "<h2>" + "Спасибо, " + purchase.Person + ", за покупку! " + "</h2>" + "ты купил книгу: "  
+                + "<br>" + " Автор: " + list[purchase.BookId - 1].Author
+                + "<br>" + " Название книги:" + list[purchase.BookId - 1].Name +
+                "<br>" + "Она стоила: " + list[purchase.BookId - 1].Price + "евро"
+                + "<br>" + " Время и дата покупки: " + purchase.Date
+                + "<br>" + " Книга будет доставлена на адресс: " + purchase.Address + "<br>";
+        }
+        public FileResult GetFile()
+        {
+            // Путь к файлу
+            string file_path = Server.MapPath("~/Files/PDFIcon.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательно
+            string file_name = "PDFIcon.pdf";
+            return File(file_path, file_type, file_name);
+        }
+        // Отправка массива байтов
+        public FileResult GetBytes()
+        {
+            string path = Server.MapPath("~/Files/PDFIcon.pdf");
+            byte[] mas = System.IO.File.ReadAllBytes(path);
+            string file_type = "application/pdf";
+            string file_name = "PDFIcon.pdf";
+            return File(mas, file_type, file_name);
+        }
+        // Отправка потока
+        public FileResult GetStream()
+        {
+            string path = Server.MapPath("~/Files/PDFIcon.pdf");
+            // Объект Stream
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "application/pdf";
+            string file_name = "PDFIcon.pdf";
+            return File(fs, file_type, file_name);
+        }
+        public string ContextData()
+        {
+            HttpContext.Response.Write("<h1>Hello World</h1>");
+
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
+        }
+        public string Indexx()
+        {
+            string browser = HttpContext.Request.Browser.Browser;
+            string user_agent = HttpContext.Request.UserAgent;
+            string url = HttpContext.Request.RawUrl;
+            string ip = HttpContext.Request.UserHostAddress;
+            string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
+            return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
+                "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
         }
     }
 }
